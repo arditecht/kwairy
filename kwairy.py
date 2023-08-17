ba7e6d94f1cfd13ce0675aff8cf3cccdb0643e83
Somjeet Dasgupta <rickdasgupta9@gmail.com>
2023-08-17 09:43:20 +0000

    added dev notes and started working on flow

--- kwairy.py
+++ kwairy.py
@@ -2,15 +2,17 @@ import os
 import openai
 
 # access/create the .env file in the project dir for getting API keys. Create a .env file in the project/repository root,
-# and add your own API key like "OPENAI_API_KEY = <your key>" without quotes, after you pull this code.
+# and add your own API key like "OPENAI_API_KEY = <your key>" without any quotes, after you pull this code in your IDE (VS Code devcontainer recommended).
 # .env has already been added to git ignore so don't worry when pushing all files to remote.
 from dotenv import load_dotenv
 load_dotenv()
 
-# libraries for querying pipeline:
+# libraries for this querying pipeline:
 from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, select, column
-from llama_index import LLMPredictor, ServiceContext, SQLDatabase, SimpleDirectoryReader, StorageContext, VectorStoreIndex
-from llama_index import set_global_service_context
+from llama_index import LLMPredictor, ServiceContext, \
+    					SQLDatabase, SimpleDirectoryReader, \
+                        StorageContext, VectorStoreIndex, \
+                        set_global_service_context
 from llama_index.indices.struct_store import SQLTableRetrieverQueryEngine
 from llama_index.objects import SQLTableNodeMapping, ObjectIndex, SQLTableSchema
 from llama_index.retrievers import VectorIndexRetriever
